297f4cf984bf3f641e1dd4e4076eef0166f4456d
tonyrobbin <dasireddy777@gmail.com>
2018-08-07 11:27:35 +0530

    pom.xml

---
+++ shopping/src/main/java/dasireddy/App.java
@@ -0,0 +1,13 @@
+package dasireddy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
